d65464d7b2c4195320400190661e2eb70af9e21a
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-02-20 23:07:05 +0100

    remove extra https

--- docker/matomo-settings.ts
+++ docker/matomo-settings.ts
@@ -2,6 +2,6 @@
  * Matomo settings for tracking statistics. This file could be mounted in the docker container.
  */
 export const matomoSettings = {
-  hostUrl: 'https://https://clarino-matomo.bgo1.test.rail.uib.no/',
+  hostUrl: 'https://clarino-matomo.bgo1.test.rail.uib.no/',
   siteId: '1'
 };
