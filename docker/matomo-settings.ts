b7532a11bddeb72f6dde4ba8e2658ee39fdcb951
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-02-20 22:58:53 +0100

    update config.yml and matomo-settings.ts

--- docker/matomo-settings.ts
+++ docker/matomo-settings.ts
@@ -2,6 +2,6 @@
  * Matomo settings for tracking statistics. This file could be mounted in the docker container.
  */
 export const matomoSettings = {
-  hostUrl: 'http://clarino-matomo:8135/',
+  hostUrl: 'https://https://clarino-matomo.bgo1.test.rail.uib.no/',
   siteId: '1'
 };
