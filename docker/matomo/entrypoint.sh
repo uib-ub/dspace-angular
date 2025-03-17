b0f263fc94cc4c3a39f666635237da732928d7ee
Øyvind Gjesdal <oyvind.gjesdal@uib.no>
2025-03-17 13:32:27 +0100

    move entrypoint from 5-apache and reuse in own entrypoint, to add symlink

---
+++ docker/matomo/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+# Run the original entrypoint
+/original-entrypoint.sh
+
+# add symlink to config, not available before mounting
+ln -sf /config/config.ini.php /var/www/html/config/config.ini.php
