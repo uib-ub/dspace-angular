3fb57f17d9e8c05ae632aa302ea374dab115148e
Amad Ul Hassan <hassan@ufal.mff.cuni.cz>
2026-04-27 11:44:30 +0200

    add New version action for archived submission items (ufal/dspace-angular#130)
    * add New version action for archived submission items
    
     add New version action for archived submission items
    
    * Use shareReplay to cache version draft observable
    
    Import shareReplay and apply .pipe(shareReplay(1)) to the version draft observable in ItemActionsComponent. This shares the subscription and caches the latest emission so multiple subscribers won't trigger duplicate work or requests.
    
    * Derive new-version tooltip from disable state
    
    Use the disableNewVersion$ observable to compute newVersionTooltip$ (with map) and apply shareReplay to disableNewVersion$, instead of calling getVersioningTooltipMessage unconditionally. This avoids invoking the tooltip service when the button is disabled. Added map import and updated unit test to assert the tooltip is derived from the disabled state and that getVersioningTooltipMessage is not called.

--- src/app/shared/mydspace-actions/item/item-actions.component.ts
+++ src/app/shared/mydspace-actions/item/item-actions.component.ts
@@ -1,6 +1,8 @@
 import { Component, Injector, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable, of as observableOf } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { MyDSpaceActionsComponent } from '../mydspace-actions';
 import { ItemDataService } from '../../../core/data/item-data.service';
 import { Item } from '../../../core/shared/item.model';
@@ -8,6 +10,10 @@ import { NotificationsService } from '../../notifications/notifications.service'
 import { RequestService } from '../../../core/data/request.service';
 import { SearchService } from '../../../core/shared/search/search.service';
 import { getItemPageRoute } from '../../../item-page/item-page-routing-paths';
+import { AuthorizationDataService } from '../../../core/data/feature-authorization/authorization-data.service';
+import { FeatureID } from '../../../core/data/feature-authorization/feature-id';
+import { DsoVersioningModalService } from '../../dso-page/dso-versioning-modal-service/dso-versioning-modal.service';
+import { hasValue } from '../../empty.util';
 
 /**
  * This component represents mydspace actions related to Item object.
@@ -31,6 +37,21 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
   itemPageRoute: string;
 
   /**
+   * Whether the current user can create a new version for this item.
+   */
+  canCreateVersion$: Observable<boolean>;
+
+  /**
+   * Whether the New version button should be disabled.
+   */
+  disableNewVersion$: Observable<boolean>;
+
+  /**
+   * Tooltip key for the New version button.
+   */
+  newVersionTooltip$: Observable<string>;
+
+  /**
    * Initialize instance variables
    *
    * @param {Injector} injector
@@ -45,12 +66,15 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
               protected notificationsService: NotificationsService,
               protected translate: TranslateService,
               protected searchService: SearchService,
-              protected requestService: RequestService) {
+              protected requestService: RequestService,
+              protected authorizationService: AuthorizationDataService,
+              protected dsoVersioningModalService: DsoVersioningModalService) {
     super(Item.type, injector, router, notificationsService, translate, searchService, requestService);
   }
 
   ngOnInit(): void {
     this.initPageRoute();
+    this.initVersioningControls();
   }
 
   /**
@@ -61,6 +85,7 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
   initObjects(object: Item) {
     this.object = object;
     this.initPageRoute();
+    this.initVersioningControls();
   }
 
   /**
@@ -68,6 +93,35 @@ export class ItemActionsComponent extends MyDSpaceActionsComponent<Item, ItemDat
    */
   initPageRoute() {
     this.itemPageRoute = getItemPageRoute(this.object);
+  }
+
+  /**
+   * Initialize authorization and button state for version creation.
+   */
+  initVersioningControls(): void {
+    this.canCreateVersion$ = observableOf(false);
+    this.disableNewVersion$ = observableOf(false);
+    this.newVersionTooltip$ = observableOf('item.page.version.create');
+
+    if (!hasValue(this.object?.self) || !hasValue(this.object?._links?.version?.href)) {
+      return;
+    }
+
+    this.canCreateVersion$ = this.authorizationService.isAuthorized(
+      FeatureID.CanCreateVersion,
+      this.object.self,
+    );
+    this.disableNewVersion$ = this.dsoVersioningModalService.isNewVersionButtonDisabled(this.object).pipe(shareReplay(1));
+    this.newVersionTooltip$ = this.disableNewVersion$.pipe(
+      map((isDisabled: boolean) => (isDisabled ? 'item.page.version.hasDraft' : 'item.page.version.create')),
+    );
+  }
+
+  /**
+   * Open the existing Create version modal for the current item.
+   */
+  openCreateVersionModal(): void {
+    this.dsoVersioningModalService.openCreateVersionModal(this.object);
   }
 
 }
